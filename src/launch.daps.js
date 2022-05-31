9f579048ce82350d7c57296736d5b3a4ac229283
Simon Petrac <pfoerdie@gmail.com>
2022-05-31 10:09:31 +0200

    added comments to launch file

--- src/launch.daps.js
+++ src/launch.daps.js
@@ -7,6 +7,8 @@ const
 
 (async function LaunchDAPS() {
 
+    /* 1. Construct a server agent for your setup: */
+
     const dapsAgent = await DAPSAgent.create({
         schema:   config.server.schema,
         hostname: config.server.hostname,
@@ -18,10 +20,16 @@ const
         domain:   true
     });
 
+    /* 2. Use additional methods to configure the setup: */
+
+    /* 3. Launch the main app: */
+
     await DAPSApp({
         'config': config,
         'agent':  dapsAgent
     });
+
+    /* 4. Launch the testing lab: */
 
     await DAPSLab({
         'config': config,
@@ -29,7 +37,11 @@ const
     });
 
 })().catch((err) => {
+
+    /* ERR. Log any error during launch and exit the application: */
+
     util.logError(err);
     debugger;
     process.exit(1);
+
 }); // LaunchDAPS
