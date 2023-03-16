b1e82a7b907f2011993f6aebc9e4f894074aa1e3
Simon Petrac <pfoerdie@gmail.com>
2023-03-16 17:13:54 +0100

    added FIWARE connector certificates

---
+++ data/connector/FIWARE/platform-kim/connector.js
@@ -0,0 +1,10 @@
+const fs           = require('fs'), path = require('path'), crypto = require('crypto');
+const load         = (filename) => fs.readFileSync(path.join(__dirname, filename));
+exports.meta       = require('./connector.json');
+exports.key        = load('./connector.key');
+exports.privateKey = crypto.createPrivateKey(exports.key);
+exports.pub        = load('./connector.pub');
+exports.publicKey  = crypto.createPublicKey(exports.pub);
+exports.cert       = load('./connector.cert');
+exports.ca         = load('./connector.ca');
+Object.freeze(exports);
