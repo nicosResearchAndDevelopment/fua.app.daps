3180a0562002150aff7146dd5c399d515352a03e
Simon Petrac <pfoerdie@gmail.com>
2023-06-14 11:09:23 +0200

    added core.app v1.0.2

--- src/launch.daps.js
+++ src/launch.daps.js
@@ -1,59 +1,38 @@
 #!/usr/bin/env node
 
-const
-    config    = require('./config/config.daps.js'),
-    util      = require('./code/util.daps.js'),
-    // BasicAuth = require('@nrd/fua.agent.amec/BasicAuth'),
-    // DatAuth   = require('@nrd/fua.agent.amec/DatAuth'),
-    DAPSAgent = require('./code/agent.daps.js'),
-    DAPSApp   = require('./app.daps.js'),
-    DAPSLab   = require('./lab.daps.js');
+// const
+//     BasicAuth = require('@nrd/fua.agent.amec/BasicAuth'),
+//     DatAuth   = require('@nrd/fua.agent.amec/DatAuth');
 
-(async function LaunchDAPS() {
-
-    /* 1. Construct a server agent for your setup: */
-
-    const dapsAgent = await DAPSAgent.create({
-        uri:      config.space.uri,
-        schema:   config.server.schema,
-        hostname: config.server.hostname,
-        port:     config.server.port,
-        context:  config.space.context,
-        store:    config.space.store,
-        server:   config.server.options,
-        daps:     config.daps,
-        app:      true,
-        io:       true,
-        domain:   true
-        // amec:     true
-    });
-
-    /* 2. Use additional methods to configure the setup: */
-
-    // TODO configure amec correctly
-    // dapsAgent.amec.registerMechanism(BasicAuth.prefLabel, BasicAuth({domain: dapsAgent.domain}));
-    // dapsAgent.amec.registerMechanism(DatAuth.prefLabel, DatAuth({connector: dapsAgent.connector}));
-
-    /* 3. Launch the main app: */
-
-    await DAPSApp({
-        'config': config,
-        'agent':  dapsAgent
-    });
-
-    /* 4. Launch the testing lab: */
-
-    if (!util.NODE_PROD) await DAPSLab({
-        'config': config,
-        'agent':  dapsAgent
-    });
-
-})().catch((err) => {
-
-    /* ERR. Log any error during launch and exit the application: */
-
-    util.logError(err);
-    debugger;
-    process.exit(1);
-
-}); // LaunchDAPS
+require('@nrd/fua.core.app').launch({
+    config: {
+        default: require('./config/config.daps.js')
+    },
+    agent:  {
+        class:  require('./code/agent.daps.js'),
+        param:  {
+            app:    true,
+            io:     true,
+            domain: true
+        },
+        mapper: (config) => ({
+            uri:      config.space.uri,
+            schema:   config.server.schema,
+            hostname: config.server.hostname,
+            port:     config.server.port,
+            context:  config.space.context,
+            store:    config.space.store,
+            server:   config.server.options,
+            daps:     config.daps
+        }),
+        async setup({agent, config}) {
+            // TODO configure amec correctly
+            // dapsAgent.amec.registerMechanism(BasicAuth.prefLabel, BasicAuth({domain: dapsAgent.domain}));
+            // dapsAgent.amec.registerMechanism(DatAuth.prefLabel, DatAuth({connector: dapsAgent.connector}));
+        }
+    },
+    app:    {
+        launch:  require('./app.daps.js'),
+        develop: require('./lab.daps.js')
+    }
+});
